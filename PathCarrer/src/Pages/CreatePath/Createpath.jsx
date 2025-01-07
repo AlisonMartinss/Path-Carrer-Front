9b232728deb46a3a780fa6a0ffcd987c2e941ed8
Alison Martins <alisonmarteso@gmail.com>
2025-01-06 22:40:57 -0300

    Organizando layout do HTML

--- PathCarrer/src/Pages/CreatePath/Createpath.jsx
+++ PathCarrer/src/Pages/CreatePath/Createpath.jsx
@@ -9,51 +9,50 @@ import Button from '../../Components/Button/Button'
 function CreatePath () {
     return (
         <main className={styles.main}>
-            <header className={styles.header}>
-                <CabecalhoPadrao/>
-            </header>
-            <div className={styles.core}>
+         <header className={styles.header}>
+            <CabecalhoPadrao/>
+         </header>
+         <div className={styles.core}>
             <form className={styles.form}>
-                <div className={styles.inputsP_core}>
-                    <div className={styles.inputP}>
-                        <TXTinputP
-                        name={"name"}
-                        id={"name"}
-                        placeholder={"Crie um nome para seu Path"}/>
-                    </div>
-                    <div className={styles.boxInput}>
-                        <BoxInput
-                            message={"Selecione uma categoria que se encaixa no seu Path:"}
-                            v1={"marketing"} txt1={"marketing"}
-                            v2={"TI"} txt2={"TIg"}
-                            v3={"Negocios"} txt3={"Negocios"}
-                        />
-                    </div>
-                    <div className={styles.inputM}>
-                        <div className={styles.paraM}>
-                            <TXTinputM
-                                message={`Digite tags que seus potenciais viwers usariam para procurar seu conteudo Ex: 
-                                #Como usar o PathCarrer #Modulos`}
+            <div className={styles.inputsP_core}>
+                <div className={styles.inputP}>
+                <TXTinputP
+                    name={"name"}
+                    id={"name"}
+                    placeholder={"Crie um nome para seu Path"}/>
+                </div>
+                <div className={styles.boxInput}>
+                <BoxInput
+                    message={"Selecione uma categoria que se encaixa no seu Path:"}
+                    v1={"marketing"} txt1={"marketing"}
+                    v2={"TI"} txt2={"TIg"}
+                    v3={"Negocios"} txt3={"Negocios"}
+                 />
+                </div>
+                <div className={styles.inputM}>
+                <div className={styles.paraM}>
+                <TXTinputM
+                    message={`Digite tags que seus potenciais viwers usariam para procurar seu conteudo Ex: 
+                    #Como usar o PathCarrer #Modulos`}
                           
-                                placeholder={"#Desenvolvimento pessoal   #Marketing Digital ..."}
-                            />
-                        </div>
-                      
-                    </div>
+                    placeholder={"#Desenvolvimento pessoal   #Marketing Digital ..."}
+                    />
+                </div>
+                </div>
                
                 </div>
                 <div className={styles.inputsG_core}>
-                    <div className={styles.inputsGG_core}>
-                        <TXTinputM
-                        placeholder={`Adicione uma descrição ao seu Path`}/>
-                    </div>
+                <div className={styles.inputsGG_core}>
+                 <TXTinputM
+                 placeholder={`Adicione uma descrição ao seu Path`}/>
+                </div>
 
-                    <Button
-                    message={"Enviar"}/>
+                 <Button
+                 message={"Enviar"}/>
                 </div>
             </form> 
-            </div>
-        </main>
+        </div>
+    </main>
     )
 }
 export default CreatePath
