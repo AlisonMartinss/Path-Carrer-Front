98f5202c5fdc0ce7ec8849d3bba4bafb99b3f8dc
Alison Martins <alisonmarteso@gmail.com>
2025-01-11 20:50:19 -0300

    Explorer finalizado

--- PathCarrer/src/Pages/Explorer/Explorer.jsx
+++ PathCarrer/src/Pages/Explorer/Explorer.jsx
@@ -2,11 +2,13 @@ import styles from '../Explorer/Explorer.module.css'
 
 import CabecalhoPadrao from '../../Components/Cabecalho/CabecalhoPadrao'
 import Conteudo from '../../Components/Conteudo/Conteudo'
+import SearchInput from '../../Components/SearchInput/SearchInput'
 
 function Explorer (){
     return (
         <main className={styles.main}>
             <header className={styles.header}><CabecalhoPadrao/></header>
+            <div className={styles.search}><SearchInput/></div>
             <div className={styles.core}>
                 <div className={styles.coreContent}>
                  <div className={styles.content}><Conteudo tituloModulo={"INSERINDO EXEMPLAR DE UM TITULO PARA O CONTEUDO QUE A DE VIR"}/></div>
@@ -22,6 +24,36 @@ function Explorer (){
                  <div className={styles.content}><Conteudo/></div>
                  <div className={styles.content}><Conteudo/></div>
                 </div>     
+
+                <div className={styles.coreContent}>
+                 <div className={styles.content}><Conteudo tituloModulo={"INSERINDO EXEMPLAR DE UM TITULO PARA O CONTEUDO QUE A DE VIR"}/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                </div> 
+
+                <div className={styles.coreContent}>
+                 <div className={styles.content}><Conteudo tituloModulo={"INSERINDO EXEMPLAR DE UM TITULO PARA O CONTEUDO QUE A DE VIR"}/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                 <div className={styles.content}><Conteudo/></div>
+                </div> 
             </div>
         </main>
     )
