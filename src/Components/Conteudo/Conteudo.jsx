cf2825d2685edd838d2df9edbe750e1b2d187471
Alison Martins <alisonmarteso@gmail.com>
2025-05-17 01:55:54 -0300

    Banner do path default

--- src/Components/Conteudo/Conteudo.jsx
+++ src/Components/Conteudo/Conteudo.jsx
@@ -3,14 +3,16 @@ import ButtonIMG from '../ButtonIMG/ButtonIMG'
 
 import { HiMiniUserGroup } from "react-icons/hi2";
 
-// Fazer ajuste em element color
+// Midia 
+
+import bannerpathDefault from '../../assets/Midias/PNGs/images/bannerpathDefault.png'
 
 
-function Conteudo ({img,adjectives,PathName,Category,onClick,views}){
+function Conteudo ({img,PathName,Category,onClick,views}){
     return (
         <div onClick={onClick} className={styles.main}>
             <div className={styles.thumbMail}>
-                <img className={styles.img} src={img} alt="" />
+                <img className={styles.img} src={img !== null && img !== undefined && img !== "" ? ({img}) : bannerpathDefault} alt="" />
             </div>
             <div className={styles.coreArea}>
                 <div className={styles.title_category}>
@@ -22,24 +24,6 @@ function Conteudo ({img,adjectives,PathName,Category,onClick,views}){
                         { views !== null && views !== undefined ? (`${views} já adicionaram esse Path`): "indefinido"}
                     </div>
                 </div>
-                {/*<div className={`${styles.adjectivesArea}`}>                        
-                        <div className={styles.adjectiveLines}>
-                            <div className={`${styles.ajdLine} ${styles.txt3}`}>
-                                {adjectives.slice(0,3).map((element) => (
-                                    <div className={`${styles.adjective} ${styles.color}`}>
-                                        {element.name}
-                                    </div>
-                                ))}
-                            </div>
-                            <div className={`${styles.ajdLine} ${styles.txt3}`}>
-                                {adjectives.slice(3,5).map((element) => (
-                                    <div className={`${styles.adjective} ${styles.color}`}>
-                                        {element.name}
-                                    </div>
-                                ))}
-                            </div>
-                        </div>                       
-                </div>*/}
             </div>
         </div>
     )
