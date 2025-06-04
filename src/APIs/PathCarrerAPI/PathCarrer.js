733aed4d73955b4bf6433ff1db0ef8b372302727
Alison Martins <alisonmarteso@gmail.com>
2025-06-04 01:49:44 -0300

    Repaginação da HomePage B

--- src/APIs/PathCarrerAPI/PathCarrer.js
+++ src/APIs/PathCarrerAPI/PathCarrer.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 const apiURL = import.meta.env.VITE_API_URL
 //http://localhost:8080/
 
-const httpClient = axios.create({baseURL:'http://localhost:8080/'})
+const httpClient = axios.create({baseURL:apiURL})
 
 export default httpClient;
